48d3802a3f0e2ce352d0ec63308452cf3079ec72
massan7251 <kurafgo9999@gmail.com>
2021-02-24 15:07:53 +0900

    ログイン、新規登録ページレイアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+  belongs_to :gender
+  belongs_to :job
+
+  with_options numericality: { other_than: 1 } do
+  validates :prefecture_id
+  validates :gender_id
+  validates :Job_id
+  end
 end
