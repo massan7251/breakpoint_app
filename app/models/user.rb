e56f0d73dc8947374d6840a0b135ee6ef4b6c887
massan7251 <kurafgo9999@gmail.com>
2021-02-24 18:15:59 +0900

    ユーザー管理機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,11 @@ class User < ApplicationRecord
   belongs_to :gender
   belongs_to :job
 
-  with_options presence: true do
-    validates :email
-    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze }
+    with_options presence: true do
     validates :name, length: { maximum: 10 }
     validates :birthday
   end
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze }
 
   with_options numericality: { other_than: 1 } do
   validates :prefecture_id
