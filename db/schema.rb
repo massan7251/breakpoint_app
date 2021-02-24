5164c08d0efc5a522cc23a562ddf81c6c01050a1
massan7251 <kurafgo9999@gmail.com>
2021-02-24 12:06:39 +0900

    devise導入

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_033735) do
+ActiveRecord::Schema.define(version: 2021_02_23_082749) do
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "agenda", null: false
@@ -25,6 +25,23 @@ ActiveRecord::Schema.define(version: 2021_02_23_033735) do
     t.string "stanceB", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "name", null: false
+    t.date "birthday", null: false
+    t.integer "gender_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "job_id", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "rooms", "stances"
