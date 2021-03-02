ccf9611997917967a35845ea76a7c304a183ab07
massan7251 <kurafgo9999@gmail.com>
2021-03-02 10:26:23 +0900

    DB修正

--- db/migrate/20210226001759_create_stances.rb
+++
@@ -1,8 +0,0 @@
-class CreateStances < ActiveRecord::Migration[6.0]
-  def change
-    create_table :stances do |t|
-      t.integer :select_stance
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -22,21 +22,6 @@ ActiveRecord::Schema.define(version: 2021_02_26_004547) do
     t.index ["user_id"], name: "index_rooms_on_user_id"
   end
 
-  create_table "stances", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "select_stance"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "user_rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "room_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["room_id"], name: "index_user_rooms_on_room_id"
-    t.index ["user_id"], name: "index_user_rooms_on_user_id"
-  end
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -55,6 +40,4 @@ ActiveRecord::Schema.define(version: 2021_02_26_004547) do
   end
 
   add_foreign_key "rooms", "users"
-  add_foreign_key "user_rooms", "rooms"
-  add_foreign_key "user_rooms", "users"
 end
