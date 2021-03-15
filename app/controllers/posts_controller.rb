512f529799fb1e4dbb3a0e6f230464d453c6acac
massan7251 <kurafgo9999@gmail.com>
2021-03-15 15:37:31 +0900

    画像投稿機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,14 +2,14 @@ class PostsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    post = Post.create(post_params)
-    redirect_to "/rooms/#{post.room.id}"
+    @post = Post.create(post_params)
+    redirect_to "/rooms/#{@post.room.id}"
   end
 
   def destroy
-    post = Post.find(params[:id])
-    post.destroy
-    redirect_to "/rooms/#{post.room.id}"
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to "/rooms/#{@post.room.id}"
   end
 
   private
